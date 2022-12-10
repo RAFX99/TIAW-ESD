8cbc375d048d3d0166397777a783a415b53e673e
Rafael Silva <85958187+RAFX99@users.noreply.github.com>
2022-12-10 09:50:50 -0300

    Revert "Filtro de busca adicionado"

--- codigo/JavaScript/Busca/dadosPesquisa/cidades.js
+++
@@ -1,7 +0,0 @@
-const cidades = [
-    { cidade: "Rio de Janeiro", abreviacao: "rio", link:"riojaneiro" },
-    { cidade: "São paulo", abreviacao: "sao" , link:"saopaulo"},
-    { cidade: "Maranhão", abreviacao: "maranhao" , link:"maranhao"},
-    { cidade: "Santa catarina", abreviacao: "santa" , link:"rio"},
-    { cidade: "Rio de Janeiro", abreviacao: "rio" ,link:"rio" },
-];
